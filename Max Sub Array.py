ff763385ecda317f8f99fecd86a7d847988ef850
Mohit Motwani <mohitmmotwani54@gmail.com>
2017-09-10 07:18:41 +0800

    Maximum SubArray 2

--- Max Sub Array.py
+++ Max Sub Array.py
@@ -30,7 +30,7 @@ def find_max_subarray(array, low, high):
         return low, high, array[low]
     else:
         # Dividing
-        mid = (low + high) / 2
+        mid = (low + high) // 2
         left_low, left_high, left_sum = find_max_subarray(array, low, mid)
         right_low, right_high, right_sum = find_max_subarray(array, mid + 1, high)
         cross_low, cross_high, cross_sum = find_max_crossing_subarray(array, low, mid, high)
@@ -46,8 +46,8 @@ def find_max_subarray(array, low, high):
 
 a = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
 low, high, Max_sum = find_max_subarray(a, 0, len(a) - 1)
-print low + 1  # Print lowest index of max sub-array
-print high + 1  # Print highest index of max sub-array
-print Max_sum  # Print sum of max sub-array
-print find_max_crossing_subarray.__doc__
-print find_max_subarray.__doc__
+print(low + 1)  # Print lowest index of max sub-array
+print(high + 1)  # Print highest index of max sub-array
+print(Max_sum)  # Print sum of max sub-array
+print(find_max_crossing_subarray.__doc__)
+print(find_max_subarray.__doc__)
---
+++ Maximum Sub Array.py
@@ -0,0 +1,42 @@
+def find_max_crossing_subarray(A, low, mid, high):
+    left_sum = float("-inf")
+    right_sum = float("-inf")
+    max_left = float("inf")
+    max_right = float("inf")
+    sum = 0
+    for i in range(mid, low - 1, -1):
+        sum = sum + A[i]
+        if sum > left_sum:
+            left_sum = sum
+            max_left = i
+
+    sum = 0
+    for j in range(mid + 1, high + 1, 1):
+        sum = sum + A[j]
+        if sum > right_sum:
+            right_sum = sum
+            max_right = j
+
+    return max_left, max_right, left_sum + right_sum
+
+
+def find_max_subarray(A, low, high):
+    if low == high:
+        return low, high, A[low]
+    else:
+        mid = (low + high) // 2
+
+        left_low, left_high, left_sum = find_max_subarray(A, low, mid)
+        right_low, right_high, right_sum = find_max_subarray(A, mid + 1, high)
+        cross_low, cross_high, cross_sum = find_max_crossing_subarray(A, low, mid, high)
+
+    if left_sum >= right_sum and left_sum >= cross_sum:
+        return left_low, left_high, left_sum
+    elif right_sum > left_sum and right_sum >= cross_sum:
+        return right_low, right_high, right_sum
+    else:
+        return cross_low, cross_high, cross_sum
+
+
+a = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+print(find_max_subarray(a, 0, len(a) - 1))
